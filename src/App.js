2152f54aaf0a65d23a15b29db406c893c78b94d7
sammykuria <kungukuria5724@gmail.com>
2025-11-01 11:15:20 +0300

    implimented an invisible button

--- src/App.js
+++ src/App.js
@@ -16,8 +16,24 @@ import Footer from './components/Footer';
 import BookAppointment from './components/BookAppointment';
 import DoctorDetails from './components/DoctorDetails';
 import AddBlog from './components/AddBlog';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function App() {
+
+   const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleKey = (e) => {
+      if (e.ctrlKey && e.shiftKey && e.key === "K") {
+        navigate("/addblog");
+      }
+    };
+
+    window.addEventListener("keydown", handleKey);
+    return () => window.removeEventListener("keydown", handleKey);
+  }, [navigate]);
 
   return (
     <div className="App">
